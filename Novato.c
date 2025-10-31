0e4fd38bb0b88d077644d975610ae35422d262c8
Daniel Bof Demuner <50131059+niellzinhow@users.noreply.github.com>
2025-10-31 18:49:30 -0300

    Implement chess movements simulation in Novato.c
    This program simulates the movements of chess pieces in novice mode, demonstrating the rook, bishop, and queen movements.

---
+++ Novato.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+
+    int i, contador; //i para for e do-while e contador para while
+
+    printf("*** Bem-vindo ao Xadrez - Modo Novato! ***\n");
+    
+    //simulando o movimento da torre utilizando o for
+
+    printf("### Movimento da Torre ###\n");
+    for( i=1; i<=5; i++ ) {
+        printf("Torre movendo para a direita\n");
+    }
+
+    printf("\n");
+
+    //simulando o movimento do bispo utilizando o while
+    printf("### Movimento do Bispo ###\n");
+    contador = 1;
+    while ( contador <= 5 ) {
+        printf("Bispo movendo na diagonal direita-cima\n");
+        contador++;
+    }
+    printf("\n");
+
+//simulando o movimento da rainha utilizando o do-while
+
+    printf("### Movimento da Rainha ###\n");
+    i = 1;
+    do {
+        printf("Rainha movendo para esquerda\n");
+        i++;
+    } while ( i <= 8 );
+
+    printf("\nFim do modo Novato. Boa sorte!\n");
+    
+    return 0;
+}
